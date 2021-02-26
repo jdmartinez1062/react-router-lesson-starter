706320e1035699a5dd5f37dddd22ec8a34ce8031
jdmartinez1062 <jd.martinez1062@gmail.com>
2021-02-26 17:40:39 -0500

    added nuxt project

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,13 +5,12 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-    allow do
-      origins 'http://localhost:8080'
-  
-      resource '*',
-        headers: :any,
-        methods: [:get, :post, :put, :patch, :delete, :options, :head]
-    end
+  allow do
+    origins 'http://localhost:34661'
+
+    resource '*',
+             headers: :any,
+             methods: %i[get post put patch delete options head]
   end
+end
