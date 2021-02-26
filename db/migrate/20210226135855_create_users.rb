9fb782d39c555d42f1e821c8ab4b1789059ab661
jdmartinez1062 <jd.martinez1062@gmail.com>
2021-02-26 09:10:32 -0500

    created User model

---
+++ db/migrate/20210226135855_create_users.rb
@@ -0,0 +1,17 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :name, default: '', null: false
+      t.string :email, default: '', null: false
+      t.string :encrypted_password, default: '', null: false
+      t.string :id_type, default: '', null: false
+      t.string :id, default: '', null: false
+      t.string :jti, default: '', null: false
+
+      t.timestamps
+    end
+
+    add_index :users, :jti, unique: true
+    add_index :users, :id, unique: true
+  end
+end
